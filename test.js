6e8c3c626511422809d8ed441289b5a37387398a
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-08-12 21:30:57 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,56 +1,13 @@
-function decodeMorseCode(morseCode) {
-  // Morse code dictionary
-  const morseCodeDictionary = {
-    '.-': 'A',
-    '-...': 'B',
-    '-.-.': 'C',
-    '-..': 'D',
-    '.': 'E',
-    '..-.': 'F',
-    '--.': 'G',
-    '....': 'H',
-    '..': 'I',
-    '.---': 'J',
-    '-.-': 'K',
-    '.-..': 'L',
-    '--': 'M',
-    '-.': 'N',
-    '---': 'O',
-    '.--.': 'P',
-    '--.-': 'Q',
-    '.-.': 'R',
-    '...': 'S',
-    '-': 'T',
-    '..-': 'U',
-    '...-': 'V',
-    '.--': 'W',
-    '-..-': 'X',
-    '-.--': 'Y',
-    '--..': 'Z',
-    '.----': '1',
-    '..---': '2',
-    '...--': '3',
-    '....-': '4',
-    '.....': '5',
-    '-....': '6',
-    '--...': '7',
-    '---..': '8',
-    '----.': '9',
-    '-----': '0'
-  };
+decodeMorse = function(morseCode) {
+    outPut = "";
 
-  // Split the morse code into individual characters
-  const morseCodeArr = morseCode.trim().split(' ');
+    for (var i = 0; i < morseCode.split(" ").length; i++) {
+        if (i === "   ") {
+            outPut += " ";
+        } else {
+            outPut += MORSE_CODE[morseCode.split(" ")[i]];
+        }
 
-  // Convert each morse code character to its corresponding letter
-  const decodedStr = morseCodeArr.map(code => {
-    return morseCodeDictionary[code] || '';
-  }).join('');
-
-  return decodedStr;
+    }
+    return outPut;
 }
-
-// Example usage:
-const morseCode = "- .... .. ...   .. ...   -.-. --- -. -.-. .   .- -. -..   .-. --- .-- -.   - .... .   - .. ... -.-. .-. . -.-.--";
-const decodedStr = decodeMorseCode(morseCode);
-console.log(decodedStr); // Output: "THIS IS CONCEPT AND ROW THE TISCRETARY!"
